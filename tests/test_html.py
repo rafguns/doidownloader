986cc1045577006cf24900dadea0d68c64b976b7
Raf Guns <raf.guns@uantwerpen.be>
2024-03-26 14:55:46 +0100

    Test response_to_html

--- tests/test_html.py
+++ tests/test_html.py
@@ -61,7 +61,22 @@ def test_metadata_from_html(html_string, metadata):
 
 
 def test_response_to_html():
-    ...
+    response = httpx.Response(
+        200,
+        content=b"""<?xml version="1.0" encoding="iso-88859-1"?><html>
+            <a href="/foo">relative link</a>
+            <a href="https://example.com/bar">absolute link</a>
+            <a href="https://example.com[17/12/2017]">Issue 20</a>
+            </html>""",
+        request=httpx.Request("GET", "https://example.org"),
+    )
+    html_el = html.response_to_html(response)
+    link_els = html_el.cssselect("a[href]")
+    assert {link.attrib["href"] for link in link_els} == {
+        "https://example.org/foo",
+        "https://example.com/bar",
+        "https://example.com[17/12/2017]",
+    }
 
 
 def test_fulltext_urls_from_meta():
