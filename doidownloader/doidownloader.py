8a9c71fb3422b1e0ea926f6ecd36630b7ba6d675
Raf Guns <raf.guns@uantwerpen.be>
2026-01-29 20:53:27 +0100

    Fix flit error due to missing description/version
    Fixes #33

--- doidownloader/doidownloader.py
+++ doidownloader/doidownloader.py
@@ -1,13 +1,3 @@
-"""📝⬇️ DOIdownloader: You give it DOIs, it gives you the article PDFs.**
-
-It is surprisingly tricky to reliably obtain the full PDF of a scientific
-publication given its DOI. This Python package aims to do just that: you give it a list
-of DOIs, and it will download the full-text PDFs (or other formats if no PDF is
-available), taking care of much of the complexity. It ensures that lookups to
-different domains can happen asynchronously (i.e., one slow website won't stall all
-your other downloads).
-
-"""
 import asyncio
 import json
 import logging
